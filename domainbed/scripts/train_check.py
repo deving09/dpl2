47522bafd85d368043eea4915695cd66c285ccb4
devin <dguillory@berkeley.edu>
2023-02-12 04:34:03 +0000

    updates for interpolation

---
+++ domainbed/scripts/train_check.py
@@ -0,0 +1,437 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+import argparse
+import collections
+import json
+import os
+import random
+import sys
+import time
+import uuid
+from itertools import chain
+
+import wandb
+
+import numpy as np
+import PIL
+import torch
+import torchvision
+import torch.utils.data
+
+from domainbed import datasets
+from domainbed import hparams_registry
+from domainbed import algorithms
+from domainbed.lib import misc
+from domainbed.lib.fast_data_loader import InfiniteDataLoader, FastDataLoader, DataParallelPassthrough
+from domainbed import model_selection
+from domainbed.lib.query import Q
+
+#import omegaconf
+#from omegaconf import OmegaConf
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Domain generalization')
+    parser.add_argument('--data_dir', type=str)
+    parser.add_argument('--dataset', type=str, default="RotatedMNIST")
+    parser.add_argument('--algorithm', type=str, default="ERM")
+    parser.add_argument('--task', type=str, default="domain_generalization",
+        help='domain_generalization | domain_adaptation')
+    parser.add_argument('--hparams', type=str,
+        help='JSON-serialized hparams dict')
+    parser.add_argument('--hparams_seed', type=int, default=0,
+        help='Seed for random hparams (0 means "default hparams")')
+    parser.add_argument('--trial_seed', type=int, default=0,
+        help='Trial number (used for seeding split_dataset and '
+        'random_hparams).')
+    parser.add_argument('--seed', type=int, default=0,
+        help='Seed for everything else')
+    parser.add_argument('--steps', type=int, default=None,
+        help='Number of steps. Default is dataset-dependent.')
+    parser.add_argument('--checkpoint_freq', type=int, default=None,
+        help='Checkpoint every N steps. Default is dataset-dependent.')
+    parser.add_argument('--test_envs', type=int, nargs='+', default=[0])
+    parser.add_argument('--output_dir', type=str, default="train_output")
+    parser.add_argument('--holdout_fraction', type=float, default=0.2)
+    parser.add_argument('--uda_holdout_fraction', type=float, default=0)
+    parser.add_argument('--skip_model_save', action='store_true')
+    parser.add_argument('--save_model_every_checkpoint', action='store_true')
+    parser.add_argument("--project", default="prompt_dg", help="wandb project name")
+
+    parser.add_argument('overrides', nargs='*', help="Any key=calue arguements to override config values"
+                                                     "(use dots for.nest=overrides)")
+
+    parser.add_argument('--envs', type=str,
+        help='JSON-serialized hparams dict',
+        default="""{\"base_envs\": [[0],[1], [2], [3], [0, 1, 2, 3]],
+            \"test_envs\": [0, 1, 2, 3]    
+        }""")
+
+
+
+    # parser.add_argument('--clip_backbone', type=str, default="None")
+    args = parser.parse_args()
+
+    env_dict = json.loads(args.envs)
+
+    # If we ever want to implement checkpointing, just persist these values
+    # every once in a while, and then load them from disk here.
+    start_step = 0
+    algorithm_dict = None
+
+    os.makedirs(args.output_dir, exist_ok=True)
+    print(args.output_dir)
+    sys.stdout = misc.Tee(os.path.join(args.output_dir, 'out.txt'))
+    sys.stderr = misc.Tee(os.path.join(args.output_dir, 'err.txt'))
+    # sys.stderr = misc.Tee(os.path.join(args.output_dir, 'results.jsonl'))
+
+    print("Environment:")
+    print("\tPython: {}".format(sys.version.split(" ")[0]))
+    print("\tPyTorch: {}".format(torch.__version__))
+    print("\tTorchvision: {}".format(torchvision.__version__))
+    print("\tCUDA: {}".format(torch.version.cuda))
+    print("\tCUDNN: {}".format(torch.backends.cudnn.version()))
+    print("\tNumPy: {}".format(np.__version__))
+    print("\tPIL: {}".format(PIL.__version__))
+
+    print('Args:')
+    for k, v in sorted(vars(args).items()):
+        print('\t{}: {}'.format(k, v))
+
+
+    if args.hparams_seed == 0:
+        hparams = hparams_registry.default_hparams(args.algorithm, args.dataset)
+    else:
+        hparams = hparams_registry.random_hparams(args.algorithm, args.dataset,
+            misc.seed_hash(args.hparams_seed, args.trial_seed))
+    if args.hparams:
+        hparams.update(json.loads(args.hparams))
+
+    #hparams['test_envs'] = [int(i) for i in args.test_envs]
+    hparams['test_envs'] = [int(i) for i in env_dict["test_envs"]]
+
+    hparams['clip_transform'] = hparams['backbone'] == 'clip'
+
+    print('HParams:')
+    for k, v in sorted(hparams.items()):
+        print('\t{}: {}'.format(k, v))
+
+    args.hparams = hparams
+    
+    #run = wandb.init(project=f"{args.project}_{args.dataset}", group=args.algorithm, config=args)
+    run = wandb.init(project=f"{args.project}_{args.dataset}", group="_".join(map(str, args.test_envs)), config=args)
+
+    #wandb.save(args)
+    wandb.run.log_code(".")
+
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    if args.dataset in vars(datasets):
+        dataset = vars(datasets)[args.dataset](args.data_dir, args.test_envs, hparams)
+    else:
+        raise NotImplementedError
+
+
+    # TODO:
+    #args.skip_model_save = True
+    def save_checkpoint(filename):
+        print("SAVE CHECKPOINT: %s" % filename)
+        if args.skip_model_save:
+            print("WHY ARE YOU SKIPPING")
+            return
+        save_dict = {
+            "args": vars(args),
+            "model_input_shape": dataset.input_shape,
+            "model_num_classes": dataset.num_classes,
+            "model_num_domains": len(dataset) - len(args.test_envs),
+            "model_hparams": hparams,
+            "model_dict": algorithm.cpu().state_dict()
+        }
+        
+        #print(algorithm.cpu().state_dict().keys())
+        print("SAVE CHECKPOINT: %s" % filename)
+        print(os.path.join(args.output_dir, filename))
+        torch.save(save_dict, os.path.join(args.output_dir, filename))
+
+
+    def load_checkpoint(filename):
+        print("LOAD CHECKPOINT: %s" % filename)
+        model_dict = torch.load(os.path.join(args.output_dir, filename))["model_dict"] 
+        #print(list(model_dict.keys()))
+        #1/0
+        init_state_dict = torch.load(os.path.join(args.output_dir, filename))["model_dict"]
+        return init_state_dict
+
+    ####### SET UP EACH BASE AND ADAPTED ENVIRONMENT #########
+
+    base_envs = env_dict["base_envs"]
+    
+    ######## SAVE INITIALIZATION CHECKPOINT    ##########
+    algorithm_class = algorithms.get_algorithm_class(args.algorithm)
+    algorithm = algorithm_class(dataset.input_shape, dataset.num_classes,
+        len(dataset) - len(args.test_envs), hparams)
+
+    if algorithm_dict is not None:
+        algorithm.load_state_dict(algorithm_dict)
+
+    algorithm.to(device)
+    if hasattr(algorithm, 'network'):
+        algorithm.network = DataParallelPassthrough(algorithm.network)
+    else:
+        for m in algorithm.children():
+            m = DataParallelPassthrough(m)
+
+    #Save Initialization
+    save_checkpoint('initialization.pkl')
+    #save_checkpoint('init_model.pkl')
+    
+    #Delete ASAP
+    #algorithm.to(device)
+    
+    for base_env in base_envs:
+        base_env = [int(b) for b in base_env]
+        test_envs = env_dict["test_envs"]
+        test_envs = [int(t) for t in test_envs if int(t) not in base_env]
+        base_env_name = "_".join((str(b) for b in base_env))
+
+
+
+        def train_algs(init_name, base_env_name, test_envs):
+            
+            start_step = 0
+            
+            # Split each env into an 'in-split' and an 'out-split'. We'll train on
+            # each in-split except the test envs, and evaluate on all splits.
+            
+            # To allow unsupervised domain adaptation experiments, we split each test
+            # env into 'in-split', 'uda-split' and 'out-split'. The 'in-split' is used
+            # by collect_results.py to compute classification accuracies.  The
+            # 'out-split' is used by the Oracle model selectino method. The unlabeled
+            # samples in 'uda-split' are passed to the algorithm at training time if
+            # args.task == "domain_adaptation". If we are interested in comparing
+            # domain generalization and domain adaptation results, then domain
+            # generalization algorithms should create the same 'uda-splits', which will
+            # be discared at training.
+            in_splits = []
+            out_splits = []
+            uda_splits = []
+            for env_i, env in enumerate(dataset):
+                uda = []
+
+                out, in_ = misc.split_dataset(env,
+                    int(len(env) * args.holdout_fraction),
+                    misc.seed_hash(args.trial_seed, env_i))
+
+                #if env_i in args.test_envs:
+                if env_i in test_envs:
+                    uda, in_ = misc.split_dataset(in_,
+                        int(len(in_)*args.uda_holdout_fraction),
+                        misc.seed_hash(args.trial_seed, env_i))
+
+                if hparams['class_balanced']:
+                    in_weights = misc.make_weights_for_balanced_classes(in_)
+                    out_weights = misc.make_weights_for_balanced_classes(out)
+                    if uda is not None:
+                        uda_weights = misc.make_weights_for_balanced_classes(uda)
+                else:
+                    in_weights, out_weights, uda_weights = None, None, None
+                in_splits.append((in_, in_weights))
+                out_splits.append((out, out_weights))
+                if len(uda):
+                    uda_splits.append((uda, uda_weights))
+
+            train_loaders = [InfiniteDataLoader(
+                dataset=env,
+                weights=env_weights,
+                batch_size=hparams['batch_size'],
+                num_workers=dataset.N_WORKERS)
+                for i, (env, env_weights) in enumerate(in_splits)
+                if i not in test_envs]
+            
+            uda_loaders = [InfiniteDataLoader(
+                dataset=env,
+                weights=env_weights,
+                batch_size=hparams['batch_size'],
+                num_workers=dataset.N_WORKERS)
+                for i, (env, env_weights) in enumerate(uda_splits)
+                if i in test_envs]
+
+            #print(uda_splits)
+            #print(len(dataset))
+            #print(len(args.test_envs))
+            #print(args.test_envs)
+            #print(dataset)
+            #1/0
+
+            eval_loaders = [FastDataLoader(
+                dataset=env,
+                batch_size=64,
+                num_workers=dataset.N_WORKERS)
+                for env, _ in (in_splits + out_splits + uda_splits)]
+            eval_weights = [None for _, weights in (in_splits + out_splits + uda_splits)]
+            eval_loader_names = ['env{}_in'.format(i)
+                for i in range(len(in_splits))]
+            eval_loader_names += ['env{}_out'.format(i)
+                for i in range(len(out_splits))]
+            eval_loader_names += ['env{}_uda'.format(i)
+                for i in range(len(uda_splits))]
+
+            algorithm_class = algorithms.get_algorithm_class(args.algorithm)
+            algorithm = algorithm_class(dataset.input_shape, dataset.num_classes,
+                len(dataset) - len(test_envs), hparams)
+            #len(dataset) - len(args.test_envs), hparams)
+
+            #algorithm.load_state_dict(load_checkpoint("initialization.pkl"))
+            
+            #if algorithm_dict is not None:
+            #    algorithm.load_state_dict(algorithm_dict)
+
+            algorithm.to(device)
+            if hasattr(algorithm, 'network'):
+                algorithm.network = DataParallelPassthrough(algorithm.network)
+            else:
+                for m in algorithm.children():
+                    m = DataParallelPassthrough(m)
+
+            #algorithm.load_state_dict(load_checkpoint("initialization.pkl"))
+            algorithm.load_state_dict(load_checkpoint(init_name))
+            
+
+            train_minibatches_iterator = zip(*train_loaders)
+            uda_minibatches_iterator = zip(*uda_loaders)
+            checkpoint_vals = collections.defaultdict(lambda: [])
+
+            steps_per_epoch = min([len(env)/hparams['batch_size'] for env,_ in in_splits])
+
+            if args.algorithm in ["WordCLIP", "CLIP"]:
+                print('Setting step to 1...')
+                args.steps = 1  # do not learn anything.
+
+            n_steps = args.steps or dataset.N_STEPS
+            checkpoint_freq = args.checkpoint_freq or dataset.CHECKPOINT_FREQ
+
+
+            # Save Initialization
+            # save_checkpoint('initialization.pkl')
+            # save_checkpoint('init_model.pkl')
+            algorithm.to(device)
+
+
+            last_results_keys = None
+            for step in range(start_step, n_steps):
+                step_start_time = time.time()
+                minibatches_device = [(x.to(device), y.to(device))
+                    for x,y in next(train_minibatches_iterator)]
+                if args.task == "domain_adaptation":
+                    uda_device = [x.to(device) for x,_ in next(uda_minibatches_iterator)]
+                else:
+                    uda_device = None
+                step_vals = algorithm.update(minibatches_device, uda_device)
+                checkpoint_vals['step_time'].append(time.time() - step_start_time)
+
+                for key, val in step_vals.items():
+                    checkpoint_vals[key].append(val)
+
+                if (step % checkpoint_freq == 0) or (step == n_steps - 1):
+                    results = {
+                        'step': step,
+                        'epoch': step / steps_per_epoch,
+                    }
+
+                    for key, val in checkpoint_vals.items():
+                        results[key] = np.mean(val)
+
+                    evals = zip(eval_loader_names, eval_loaders, eval_weights)
+                    for name, loader, weights in evals:
+                        acc = misc.accuracy(algorithm, loader, weights, device)
+                        results[name+'_acc'] = acc
+
+                    results_keys = sorted(results.keys())
+                    if results_keys != last_results_keys:
+                        misc.print_row(results_keys, colwidth=12)
+                        last_results_keys = results_keys
+                    misc.print_row([results[key] for key in results_keys],
+                        colwidth=12)
+                    
+                    wandb.log(results)
+
+                    results.update({
+                        'hparams': hparams,
+                        'args': vars(args)    
+                    })
+
+                    epochs_path = os.path.join(args.output_dir, 'results.jsonl')
+
+                    with open(epochs_path, 'a') as f:
+                        f.write(json.dumps(results, sort_keys=True) + "\n")
+                   
+
+
+                    algorithm_dict = algorithm.state_dict()
+                    start_step = step + 1
+                    checkpoint_vals = collections.defaultdict(lambda: [])
+
+                    records = []
+                    with open(epochs_path, 'r') as f:
+                        for line in f:
+                            records.append(json.loads(line[:-1]))
+                    records = Q(records)
+                    scores = records.map(model_selection.IIDAccuracySelectionMethod._step_acc)
+                    #print(scores)
+                    #print(epochs_path)
+                    if scores[-1] == scores.argmax('val_acc'):
+                        save_checkpoint(f'{base_env_name}_IID_best.pkl')
+                        algorithm.to(device)
+                    
+                    if args.save_model_every_checkpoint:
+                        save_checkpoint(f'{base_env_name}_model_step{step}.pkl')          
+            
+            save_checkpoint(f'{base_env_name}_model.pkl')
+
+        #############################################
+        #   Add an adjustment for each env          #
+        #############################################
+
+        train_algs("initialization.pkl", base_env_name, test_envs)
+        
+        # Option one do joint base and test target env #
+        for current_base_env in test_envs:
+            current_test_envs = [t for t in test_envs if t != current_base_env]
+            current_base_env_name = f"adapt_env_{str(current_base_env)}"
+            train_algs(f"{base_env_name}_model.pkl", 
+                    f"{base_env_name}_base_{current_base_env_name}",
+                    current_test_envs)
+
+        # Option two do only test target env           #
+        for current_base_env in test_envs:
+            new_base_env = base_env + [current_base_env]
+            current_test_envs = [t for t in test_envs if t not in new_base_env]
+            current_base_env_name = "_".join((str(b) for b in new_base_env))            
+            train_algs(f"{base_env_name}_model.pkl", 
+                    f"{base_env_name}_base_{current_base_env_name}",
+                    current_test_envs)
+            
+
+        # Option three do all test environments together 
+        new_base_env = test_envs.copy()
+        current_test_envs = [t for t in test_envs if t not in new_base_env]
+        current_base_env_name = "_".join((str(b) for b in new_base_env))            
+        train_algs(f"{base_env_name}_model.pkl", 
+                f"{base_env_name}_base_{current_base_env_name}",
+                current_test_envs)
+
+
+
+
+    with open(os.path.join(args.output_dir, 'done'), 'w') as f:
+        f.write('done')
